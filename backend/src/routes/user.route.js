6ede5476f7a345e5b60ec9b6bcfbdb158717af26
amirrajj <amiramraja@gmail.com>
2025-07-08 15:08:55 +0330

    fixed a funny backend mistake❄️🐧

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -7,7 +7,7 @@ const router = Router()
 router.get('/profile/:username' , getUserProfile)
 router.put('/profile' , protectRoute , updateUserProfile)
 router.post('/sync' , protectRoute , syncUser)
-router.put('/me' , protectRoute , getCurrentUser)
+router.get('/me' , protectRoute , getCurrentUser)
 router.post('/follow/:userId' , protectRoute , followUnfollowUser)
 
 export default router;
